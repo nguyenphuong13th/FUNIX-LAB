fc64d49a9fe5a1689870fb967f99f4acf8d9fccd
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-02-22 00:32:34 +0700

    Components Part 1

--- Lab02.1/confusion/src/App.js
+++ Lab02.1/confusion/src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <Menu/>
           </div>
         </Navbar>
       </div>
