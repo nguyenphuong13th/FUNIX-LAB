cfc2f9263caa0ff6f32cf3f8696c9bce8492c053
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-02-26 23:58:42 +0700

    React Router

--- Lab02.1/confusion/src/components/MenuComponent.js
+++ Lab02.1/confusion/src/components/MenuComponent.js
@@ -10,8 +10,7 @@ class Menu extends Component {
       const menu = this.props.dishes.map((dish) => {
           return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
-              <Card
-                onClick={() => this.props.onClick(dish.id)}>
+              <Card>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
